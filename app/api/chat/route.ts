06fca31153ae325e989c50248dd5bfed8a32750e
Edmond Tam <edmondtam2@gmail.com>
2023-11-04 16:14:39 +0000

    Initial commit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,27 +1,28 @@
-import { StreamingTextResponse, LangChainStream, Message } from 'ai';
-import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { AIMessage, HumanMessage } from 'langchain/schema';
+import { StreamingTextResponse, LangChainStream, Message } from "ai";
+import { ChatAnthropic } from "langchain/chat_models/anthropic";
+import { AIMessage, HumanMessage } from "langchain/schema";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const { stream, handlers } = LangChainStream();
 
-  const llm = new ChatOpenAI({
+  const llm = new ChatAnthropic({
     streaming: true,
+    anthropicApiKey: process.env.ANTHROPIC_API_KEY,
   });
 
   llm
     .call(
-      (messages as Message[]).map(m =>
-        m.role == 'user'
+      (messages as Message[]).map((m) =>
+        m.role == "user"
           ? new HumanMessage(m.content)
-          : new AIMessage(m.content),
+          : new AIMessage(m.content)
       ),
       {},
-      [handlers],
+      [handlers]
     )
     .catch(console.error);
 
